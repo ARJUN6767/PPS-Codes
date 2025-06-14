d45dc255874fb9257b87a38113c1e6df736e14c2
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:58:09 +0530

    Add files via upload

---
+++ SubByTakingInput.c
@@ -0,0 +1,15 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int main()
+{
+    int n1, n2, sub;
+    printf("Enter First Number");
+    scanf("%d", &n1);
+    printf("Enter Second Number");
+    scanf("%d", &n2);
+    sub = n1 - n2;
+    printf("The subtraction of %d and %d is %d", n1, n2, sub);
+    return 0;
+
+}
