d9a7624220af0c437b48f7af13badec60a804ca3
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:51:42 +0530

    Add files via upload

---
+++ 1To10.c
@@ -0,0 +1,13 @@
+// Name:Arjun more PRN:24030331378012
+
+#include <stdio.h>
+int main() 
+{
+    printf("Numbers from 1 to 10:\n");
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
