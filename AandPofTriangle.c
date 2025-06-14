60d21077e5d248d0e6e354704ec3e43933a5bf18
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:52:15 +0530

    Add files via upload

---
+++ AandPofTriangle.c
@@ -0,0 +1,17 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int main()
+{
+    int length,breadth,area,perimeter;
+    printf("Enter the Length Of The Rectangle");
+    scanf("%d",&length);
+    printf("Enter the Breadth Of The Rectangle");
+    scanf("%d",&breadth);
+    area=length*breadth;
+    perimeter=2*(length+breadth);
+    printf("The Area Of Rectangle is %d /n he Perimeter Of Rectangle is %d",area,perimeter);
+    return 0;
+
+    
+}
