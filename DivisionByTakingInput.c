21d2cab0ad269f9a87fe2c203825eeec631ba360
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:53:48 +0530

    Add files via upload

---
+++ DivisionByTakingInput.c
@@ -0,0 +1,14 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int main()
+{
+    int n1, n2, div;
+    printf("Enter First Number");
+    scanf("%d", &n1);
+    printf("Enter Second Number");
+    scanf("%d", &n2);
+    div=n1/n2;
+    printf("The Division Of %d and %d is %d",n1, n2, div);
+    return 0;
+}
