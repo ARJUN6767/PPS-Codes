6d9eb14bebba4519d306fd667445e6b293a3ed5b
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:52:58 +0530

    Add files via upload

---
+++ Average.c
@@ -0,0 +1,16 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int main()
+{
+    int n1, n2, n3, avg;
+    printf("Enter First Number");
+    scanf("%d", &n1);
+    printf("Enter Second Number");
+    scanf("%d", &n2);
+    printf("Enter Third Number");
+    scanf("%d", &n3);
+    avg=(n1+n2+n3)/3;
+    printf("The Average Of %d and %d is %d",n1, n2, avg);
+    return 0;
+}
