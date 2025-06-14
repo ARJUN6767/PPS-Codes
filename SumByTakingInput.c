ece351bba62bc2cc407f2e702073239c9e0d25b6
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:58:41 +0530

    Add files via upload

---
+++ SumByTakingInput.c
@@ -0,0 +1,14 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int main()
+{
+    int n1, n2, sum; 
+    printf("Enter First Number");
+    scanf("%d", &n1);
+    printf("Enter Second Number");
+    scanf("%d", &n2);
+    sum = n1 + n2;  
+    printf("The Sum Of %d and %d is %d", n1, n2, sum); 
+    return 0;
+}
