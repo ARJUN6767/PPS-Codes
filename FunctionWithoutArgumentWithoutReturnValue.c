d7e7c926ead8a4feaa61145433b0b35e3142ff24
ARJUN6767 <morearjun6767@gmail.com>
2025-06-14 15:55:32 +0530

    Add files via upload

---
+++ FunctionWithoutArgumentWithoutReturnValue.c
@@ -0,0 +1,14 @@
+// Name:Arjun more PRN:24030331378012
+
+#include<stdio.h>
+int addition()
+{
+    int a=5,b=2,c;
+    c=a+b;
+    printf("Addition %d",c);
+}
+int main()
+{
+    addition();
+    return 0;
+}
