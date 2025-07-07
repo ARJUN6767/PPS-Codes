c53e96f084c174125d69463f72c56347bbbe5d5e
ARJUN6767 <morearjun6767@gmail.com>
2025-07-07 23:46:29 +0530

    Add files via upload

---
+++ PrintNumber.c
@@ -0,0 +1,12 @@
+// Name:Arjun more PRN:24030331378012
+
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}
